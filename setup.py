d6563c8a386dd421341c434f922d3715940d8376
Nicolas ELIE <nicolas.elie@cnrs.fr>
2018-05-15 09:44:03 +0200

    Use setuptools ins etup and name project

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 from Cython.Build import cythonize
 
 import numpy as np
 
 ext_modules = [
     Extension(
-        "cosine",
-        ["cosine.pyx"],
+        "cosinelib.cosine",
+        ["cosinelib/cosine.pyx"],
         include_dirs=[np.get_include()],
         extra_compile_args=['/openmp'],
         extra_link_args=['/openmp']
     ),
     Extension(
-        "filter",
-        ["filter.pyx"],
+        "cosinelib.filter",
+        ["cosinelib/filter.pyx"],
         include_dirs=[np.get_include()]
     ),
     Extension(
-        "mgf",
-        ["mgf.pyx"],
+        "cosinelib.mgf",
+        ["cosinelib/mgf.pyx"],
         include_dirs=[np.get_include()]
+    ),
+    Extension(
+        "cosinelib.common",
+        ["cosinelib/common.pyx"]
     )
 ]
 
 setup(
+    name='cosinelib',
+    version='0.1',
     ext_modules=cythonize(ext_modules),
 )
