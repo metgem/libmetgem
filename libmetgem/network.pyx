531ea35fa66f87cda11e7b4dac397a2c104945bb
Nicolas ELIE <nicolas.elie@cnrs.fr>
2018-09-18 16:56:26 +0200

    Cythonizes generate_network and add it into a worker

---
+++ libmetgem/network.pyx
@@ -0,0 +1,167 @@
+# cython: language_level=3
+# distutils: language=c++
+
+cimport cython
+import numpy as np
+cimport numpy as np
+from libcpp cimport bool
+from libcpp.vector cimport vector
+from libcpp.algorithm cimport (sort, partial_sort)
+from libcpp.vector cimport vector
+from libc.math cimport fabs
+
+ctypedef struct interaction_t:
+    int source
+    int target
+    double delta_mz
+    double cosine
+    
+ctypedef struct element_t:
+    int index
+    double cosine
+  
+cdef bool compareElementsByCosine(const element_t &a, const element_t &b) nogil:
+    return a.cosine > b.cosine
+    
+cdef bool compareInteractionsByCosine(const interaction_t &a, const interaction_t &b) nogil:
+    if a.cosine > b.cosine:
+        return True
+    elif a.cosine < b.cosine:
+        return False
+    elif a.source > b.source:
+        return True
+    elif a.source < b.source:
+        return False
+    elif a.target > b.target:
+        return True
+    elif a.target < b.target:
+        return False
+    elif a.delta_mz > b.delta_mz:
+        return True
+    elif a.delta_mz < b.delta_mz:
+        return False
+    else:
+        return False
+ 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef vector[interaction_t] generate_network_nogil(float[:,:] scores_matrix,
+                                                  vector[double] mzvec,
+                                                  double pairs_min_cosine,
+                                                  int top_k,
+                                                  object callback=None) nogil:
+    cdef:
+        vector[interaction_t] interactions, interactions2
+        interaction_t inter
+        int size = mzvec.size()
+        int i, j
+        vector[element_t] row
+        element_t element
+        double cosine
+        int length
+        int x, y
+        vector[int] x_ind, y_ind
+        bool flag
+        bool has_callback = callback is not None
+    
+    for i in range(size):
+        row.clear()
+        row.reserve(size-i+1)
+        for j in range(i, size):
+            cosine = scores_matrix[i, j]
+            if cosine > pairs_min_cosine:
+                element.index = j
+                element.cosine = cosine
+                row.push_back(element)
+                
+        length = min(row.size(), top_k)
+        partial_sort(row.begin(), row.begin() + length,
+                     row.end(), compareElementsByCosine)
+        
+        for j in range(length):
+            element = row[j]
+            inter.source = i
+            inter.target = element.index
+            inter.delta_mz = mzvec[i]-mzvec[element.index]
+            inter.cosine = element.cosine
+            interactions.push_back(inter)
+            
+        if has_callback and i>0 and i % 100 == 0:
+            with gil:
+                if not callback(100):
+                    interactions.clear()
+                    return interactions
+                
+    size = interactions.size()
+    if has_callback and size % 100 != 0:
+        with gil:
+            if not callback(size % 100):
+                interactions.clear()
+                return interactions
+            callback(-size) # Negative value means new maximum
+        
+    sort(interactions.begin(), interactions.end(), compareInteractionsByCosine)
+        
+    # Top K algorithm, keep only edges between two nodes if and only if each
+    # of the node appeared in each other’s respective top k most similar nodes
+    interactions2.reserve(size)
+    for i in range(size):
+        x = interactions[i].source
+        y = interactions[i].target
+
+        x_ind.clear()
+        y_ind.clear()
+        for j in range(size):
+            if interactions[j].source == x or interactions[j].target == x:
+                x_ind.push_back(j)
+            if interactions[j].source == y or interactions[j].target == y:
+                y_ind.push_back(j)
+
+        x_ind.resize(min(top_k, x_ind.size()))
+        y_ind.resize(min(top_k, y_ind.size()))
+                
+        flag = False
+        for j in y_ind:
+            if x == interactions[j].source or x == interactions[j].target:
+                flag = True
+        if flag:
+            for j in x_ind:
+                if y == interactions[j].source or y == interactions[j].target:
+                    interactions2.push_back(interactions[i])
+                    break
+                    
+        if has_callback and i>0 and i % 100 == 0:
+            with gil:
+                if not callback(100):
+                    interactions2.clear()
+                    return interactions
+                
+    if has_callback and size % 100 != 0:
+        with gil:
+            callback(size % 100)
+            
+    return interactions2
+    
+def generate_network(float[:,:] scores_matrix, vector[double] mzvec,
+                     double pairs_min_cosine, int top_k, object callback=None):
+    cdef:
+        vector[interaction_t] interactions
+        interaction_t inter
+        int i
+        np.dtype dt = np.dtype([('Source', int),
+                                ('Target', int),
+                                ('Delta MZ', np.float32),
+                                ('Cosine', np.float32)])
+        np.ndarray array
+        
+    interactions = generate_network_nogil(scores_matrix, mzvec,
+                                          pairs_min_cosine, top_k,
+                                          callback)
+    if interactions.size() > 0:
+        array = np.empty((interactions.size(),), dtype=dt)
+        for i in range(array.shape[0]):
+            inter = interactions[i]
+            array[i] = (inter.source, inter.target, inter.delta_mz, inter.cosine)
+            
+        return array
+        
